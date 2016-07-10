b69c4db39c4e8122e0b691051908dd51781471bb
morning <morning@thecover.cn>
2016-07-10 10:10:22 +0800

    Initial Commit

---
+++ CropDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CropDemo
+//
+//  Created by limao on 16/7/10.
+//  Copyright © 2016年 limao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
